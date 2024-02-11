2b2ad32cd4eb579938e801feb49062f9a15a797e
elordegithub <justineluiselorde@gmail.com>
2024-02-11 21:38:54 +0800

    Modifying models.py & admin.py

--- thesis_search_app/thesisapp/models.py
+++ thesis_search_app/thesisapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Thesis(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    abstract = models.TextField()
+
+    def __str__(self):
+        return self.title
