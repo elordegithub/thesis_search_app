9dea913d01e44ca4b9153802481923435e4ebe94
elordegithub <justineluiselorde@gmail.com>
2024-02-11 23:59:35 +0800

    Fix the searching of thesis datas and displayed output

--- thesis_search_app/thesisapp/views.py
+++ thesis_search_app/thesisapp/views.py
@@ -3,8 +3,13 @@ from .models import Thesis
 
 def thesis_search(request):
     query = request.GET.get('q')
+    theses = None
     if query:
-        theses = Thesis.objects.filter(title__icontains=query)
-    else:
-        theses = Thesis.objects.all()
+        theses = Thesis.objects.filter(
+            title__icontains=query
+        ) | Thesis.objects.filter(
+            author__icontains=query
+        ) | Thesis.objects.filter(
+            abstract__icontains=query
+        )
     return render(request, 'thesis_search/search.html', {'theses': theses, 'query': query})
