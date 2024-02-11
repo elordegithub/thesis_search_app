5268584032f90f08bbeffba8de4d84f442ef5565
elordegithub <justineluiselorde@gmail.com>
2024-02-11 22:33:53 +0800

    Modify views.py &create  search.html

--- thesis_search_app/thesisapp/views.py
+++ thesis_search_app/thesisapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Thesis
 
-# Create your views here.
+def thesis_search(request):
+    query = request.GET.get('q')
+    if query:
+        theses = Thesis.objects.filter(title__icontains=query)
+    else:
+        theses = Thesis.objects.all()
+    return render(request, 'thesis_search/search.html', {'theses': theses, 'query': query})
