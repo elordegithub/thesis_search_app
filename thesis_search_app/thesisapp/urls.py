45fa59f73056ffbb7e3885c90358627c94c4b990
elordegithub <justineluiselorde@gmail.com>
2024-02-25 23:45:55 +0800

    Add and modify html file functionalities

--- thesis_search_app/thesisapp/urls.py
+++ thesis_search_app/thesisapp/urls.py
@@ -1,7 +1,10 @@
+# thesisapp/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.thesis_search, name='thesisapp'),
+    path('', views.landing_page, name='landing_page'),
+    path('thesis-list/', views.thesis_list, name='thesis_list'),
+    path('search/', views.search_results, name='search_results'),
 ]
-
--- thesis_search_app/thesisapp/views.py
+++ thesis_search_app/thesisapp/views.py
@@ -1,15 +1,22 @@
+# thesisapp/views.py
+
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Thesis
 
-def thesis_search(request):
+def landing_page(request):
+    return render(request, 'thesis_search/landing.html')
+
+def thesis_list(request):
+    theses = Thesis.objects.all()[:3]  # Get the first three thesis studies
+    return render(request, 'thesis_search/thesis_list.html', {'theses': theses})
+
+def search_results(request):
     query = request.GET.get('q')
-    theses = None
     if query:
-        theses = Thesis.objects.filter(
-            title__icontains=query
-        ) | Thesis.objects.filter(
-            author__icontains=query
-        ) | Thesis.objects.filter(
-            abstract__icontains=query
-        )
-    return render(request, 'thesis_search/search.html', {'theses': theses, 'query': query})
+        theses = Thesis.objects.filter(title__icontains=query) | \
+                 Thesis.objects.filter(author__icontains=query) | \
+                 Thesis.objects.filter(abstract__icontains=query)
+    else:
+        theses = []
+    return render(request, 'thesis_search/search_results.html', {'theses': theses})
